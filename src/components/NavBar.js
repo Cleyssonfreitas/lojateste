3d520d62978572edf129e53fb07948f889402230
Cleysson Freitas <cleyssonfreitas30@gmail.com>
2023-11-29 09:40:43 -0300

    criado  basico para navegação

---
+++ src/components/NavBar.js
@@ -0,0 +1,39 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../logo.svg';
+
+
+function NavBar() {
+  return (
+    
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      
+      <div className="container">
+      <img src={logo}           style={{ width: '70px', height: '70px' }} className="" alt="logo" />
+<a className="navbar-brand" href="/">Loja teste coder</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="/">Inicio</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" href="/">Promoçoes </a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" href="/">eletrodomésticos</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" href="/">informática</a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link" href="/">móveis</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+export default NavBar;
